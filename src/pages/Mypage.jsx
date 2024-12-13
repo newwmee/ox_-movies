ceb219f3535eeaf38ffcc14bf012ae0033e26105
newwmee <dmstjs365@gmail.com>
2024-12-13 21:10:58 +0900

    mission5

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./MyPage.css";
+import "./Mypage.css";
 import { useUser } from "./Login";
 import { useSupabaseAuth } from "../hooks/useSupabaseAuth"; // 추가
 import defaultProfile from "../assets/default-profile.png";
