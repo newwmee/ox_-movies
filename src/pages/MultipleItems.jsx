62c450dd696e920e51d203513b531b809803cf38
newwmee <dmstjs365@gmail.com>
2025-01-18 16:01:34 +0900

    캐러셀 수정 2차

--- src/pages/MultipleItems.jsx
+++ src/pages/MultipleItems.jsx
@@ -17,6 +17,7 @@ function MultipleItems({ movies }) {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    arrow: true,
     responsive: [
       {
         breakpoint: 1024,
@@ -44,15 +45,14 @@ function MultipleItems({ movies }) {
       <Slider {...settings}>
         {movies.map((movie) => (
           <div key={movie.id} className="px-2">
-            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            <div className="overflow-hidden">
               <img
                 src={`https://image.tmdb.org/t/p/w500${
                   movie.poster_path || "default.jpg"
                 }`} // 기본 이미지 추가
                 alt={movie.title}
-                className="w-10 h-25 object-contain "
               />
-              <div className="p-4">
+              <div className="w-10px">
                 <h3 className="text-lg font-semibold">{movie.title}</h3>
                 {movie.vote_average && (
                   <p className="text-sm text-gray-600">
